06c3a4cb638f98081fe1e87b66cedb034430b0bc
Evgenios Kastanias <eugenioska@gmail.com>
2015-10-06 16:11:20 +0300

    TypeScript Card Picker
    A TypeScript program that returns random cards from a deck. Written in
    Visual Studio 2013

---
+++ firstTypescriptProgram/app.js
@@ -0,0 +1,27 @@
+﻿var deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function () {
+        var _this = this;
+        return function () {
+            var pickedCard = Math.floor(Math.random() * 52);
+            var pickedSuit = Math.floor(pickedCard / 13);
+
+            return { suit: _this.suits[pickedSuit], card: pickedCard % 13 };
+        };
+    }
+};
+
+var cardPicker = deck.createCardPicker();
+var pickedCard = cardPicker();
+window.onload = function () {
+    var el = document.querySelector('#content');
+    el.innerHTML = "card: " + pickedCard.card + " of " + pickedCard.suit + "<br>";
+
+    var btn = document.querySelector('#new');
+    btn.addEventListener('click', function () {
+        var btncard = cardPicker();
+        el.innerHTML = el.innerHTML + "card: " + btncard.card + " of " + btncard.suit + "<br>";
+    });
+};
+//# sourceMappingURL=app.js.map
---
+++ firstTypescriptProgram/app.ts
@@ -0,0 +1,28 @@
+﻿var deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function () {
+        return () => {
+            var pickedCard = Math.floor(Math.random() * 52);
+            var pickedSuit = Math.floor(pickedCard / 13);
+
+            return { suit: this.suits[pickedSuit], card: pickedCard % 13 };
+        }
+    }
+}
+
+var cardPicker = deck.createCardPicker();
+var pickedCard = cardPicker();
+window.onload = () => {
+    var el = <HTMLInputElement>document.querySelector('#content');
+    el.innerHTML = "card: " + pickedCard.card + " of " + pickedCard.suit + "<br>";
+
+    var btn = <HTMLInputElement>document.querySelector('#new');
+    btn.addEventListener('click', function () {
+        var btncard = cardPicker();
+        el.innerHTML = el.innerHTML + "card: " + btncard.card + " of " + btncard.suit + "<br>";
+    });
+};
+
+
+
